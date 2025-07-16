09ff2e4cb6cbe05f2333e8621056b2c0573085bb
joaorceschini <jrvceschini@gmail.com>
2025-07-16 15:40:19 -0300

    feat: add project structure, provider and variables

---
+++ joao-rafael/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "ec2_ami" {
+  description = "AMI ID for EC2"
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ec2_tag_name" {
+  description = "EC2 name tag"
+  default     = "instancia-terraform-dev"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  default     = "bucket-dev-imagens-joao-rafael"
+}
+
+variable "bucket_tag_name" {
+  description = "S3 bucket name tag"
+  default     = "bucket-terraform-dev"
+}
